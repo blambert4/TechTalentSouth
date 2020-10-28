445f5fb34ee814cba54f76e4fcdade17257d6567
bradley <bradman100@gmail.com>
2020-10-27 23:29:53 -0400

    Twitter Clone part 1

---
+++ twitter/src/main/java/com/tts/twitter/configuration/ThymeLeafConfiguration.java
@@ -0,0 +1,11 @@
+package com.tts.twitter.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
+
+public class ThymeLeafConfiguration {
+    @Bean
+    public SpringSecurityDialect springSecurityDialect(){
+        return new SpringSecurityDialect();
+    }
+}
